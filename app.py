87d124fbe3ee2493adeb24e208946019c307e50b
Diieba0 <diieba50@gmail.com>
2022-10-07 08:33:09 +0200

    dashboard version1

--- app.py
+++ app.py
@@ -4,14 +4,6 @@ import requests
 
 app = Flask(__name__)
 
-# METEO_API_KEY = "d5d8636c0fa03a6714240d8bfee5ff32"
-# if METEO_API_KEY is None:
-#     # URL de test :
-#     METEO_API_URL = "https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx"
-# else:
-#     # URL avec clé :
-#     METEO_API_URL = "https://api.openweathermap.org/data/2.5/forecast?lat=48.883587&lon=2.333779&appid=" + METEO_API_KEY
-
 
 @app.route("/")
 def hello():
@@ -23,87 +15,24 @@ def dashboard():
     return render_template("dashboard.html")
 
 
-# @app.route('/api/meteo/')
-# def meteo():
-#     response = requests.get(METEO_API_URL)
-#     content = json.loads(response.content.decode('utf-8'))
-#
-#     if response.status_code != 200:
-#         return jsonify({
-#             'status': 'error',
-#             'message': 'La requête à l\'API météo n\'a pas fonctionné. Voici le message renvoyé par l\'API : {}'.format(
-#                 content['message'])
-#         }), 500
-#
-#     data = []  # On initialise une liste vide
-#     for prev in content["list"]:
-#         datetime = prev['dt'] * 1000
-#         temperature = prev['main']['temp'] - 273.15  # Conversion de Kelvin en °c
-#         temperature = round(temperature, 2)
-#         data.append([datetime, temperature])
-#
-#     return jsonify({
-#         'status': 'ok',
-#         'data': data
-#     })
-#
-#
-# from functions import extract_keywords
-#
-# NEWS_API_KEY = "5de7d028984d46de9c6cf7c961b12f7b"  # Remplacez None par votre clé NEWSAPI, par exemple "4116306b167e49x993017f089862d4xx"
-#
-# if NEWS_API_KEY is None:
-#     # URL de test :
-#     NEWS_API_URL = "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/courses/4525361/top-headlines.json"  # exemple de JSON
-# else:
-#     # URL avec clé :
-#     NEWS_API_URL = "https://newsapi.org/v2/top-headlines?sortBy=publishedAt&pageSize=100&language=fr&apiKey=" + NEWS_API_KEY
-#
-#
-# @app.route('/api/news/')
-# def get_news():
-#     response = requests.get(NEWS_API_URL)
-#
-#     content = json.loads(response.content.decode('utf-8'))
-#
-#     if response.status_code != 200:
-#         return jsonify({
-#             'status': 'error',
-#             'message': 'La requête à l\'API des articles d\'actualité n\'a pas fonctionné. Voici le message renvoyé par l\'API : {}'.format(
-#                 content['message'])
-#         }), 500
-#
-#     keywords, articles = extract_keywords(content["articles"])
-#
-#     return jsonify({
-#         'status': 'ok',
-#         'data': {
-#             'keywords': keywords[:100],  # On retourne uniquement les 100 premiers mots
-#             'articles': articles
-#         }
-#     })
-
-
 GRAND_DEBAT_API_KEY = None
 
 if GRAND_DEBAT_API_KEY is None:
     # URL de test :
-    GRAND_DEBAT_API_URL = "https://static.data.gouv.fr/resources/donnees-ouvertes-du-grand-debat-national/20200612-000534/cr-ril.json" # exemple de JSON
+    GRAND_DEBAT_API_URL = "https://static.data.gouv.fr/resources/donnees-ouvertes-du-grand-debat-national/20200612-000534/cr-ril.json"  # exemple de JSON
 else:
     # URL avec clé :
     GRAND_DEBAT_API_URL = "https://www.data.gouv.fr/fr/datasets/donnees-ouvertes-du-grand-debat-national/" + GRAND_DEBAT_API_KEY
-
 
 response = requests.get(GRAND_DEBAT_API_URL)
 
 content = json.loads(response.content.decode('utf-8'))
 
 
-from functions import extract_answers
 
+from functions import extract_answers
 @app.route('/api/debat/')
 def get_answers():
-
     if response.status_code != 200:
         return jsonify({
             'status': 'error',
@@ -122,12 +51,9 @@ def get_answers():
     })
 
 
-
 from functions import extract_themes
-
 @app.route('/api/themes/')
 def get_themes():
-
     if response.status_code != 200:
         return jsonify({
             'status': 'error',
@@ -144,26 +70,26 @@ def get_themes():
         }
     })
 
-# from functions import extract_participations
-#
-# @app.route('/api/participations/')
-# def get_themes():
-#
-#     if response.status_code != 200:
-#         return jsonify({
-#             'status': 'error',
-#             'message': 'La requête à l\'API des réunions locales n\'a pas fonctionné. Voici le message renvoyé par l\'API : {}'.format(
-#                 content['message'])
-#         }), 500
-#
-#     participation = extract_participations(content)
-#
-#     return jsonify({
-#         'status': 'ok',
-#         'data': {
-#             'participation': participation,
-#         }
-#     })
+
+from functions import extract_participation
+@app.route('/api/participation/')
+def get_participation():
+    if response.status_code != 200:
+        return jsonify({
+            'status': 'error',
+            'message': 'La requête à l\'API des réunions locales n\'a pas fonctionné. Voici le message renvoyé par l\'API : {}'.format(
+                content['message'])
+        }), 500
+
+    participation = extract_participation(content)
+
+    return jsonify({
+        'status': 'ok',
+        'data': {
+            'participation': participation,
+        }
+    })
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/mydashboard.js
+++ static/mydashboard.js
@@ -1,160 +1,6 @@
 // contient les articles de presse, qui doivent être
 // gardés en mémoire même après affichage du graphique
 
-// Palette de couleurs utilisée par tous les graphiques
-var colors = ["#c503fa", "#d259f5", "#e09ef3", "#f1d4fa", "#838080FF"];
-/*
-
-// Chargement des articles de presse
-$.ajax({
-    url: "/api/news",
-    success: display_news
-});
-
-var news_data;
-
-function display_news(result) {
-    news_data = result["data"];
-    display_wordcloud(news_data);
-    display_all_articles();
-}
-
-function display_wordcloud(news_data) {
-    var data = [];
-    var keywords = news_data["keywords"];
-
-    for (i in keywords) {
-        data.push({
-            name: keywords[i]["word"],
-            weight: keywords[i]['cnt'],
-            events: {
-                click: function (event) {
-                    var keyword = event.point.name;
-                    display_articles_from_word(keyword);
-                }
-            }
-        })
-    }
-    console.log("Voici les données formatées pour Highcharts :", data);
-
-    Highcharts.chart('nuage', {
-        series: [{
-            type: 'wordcloud',
-            data: data,
-            name: 'Occurrences',
-            colors:
-            colors,
-            rotation: {
-                from: -60,
-                to: 60,
-                orientations: 8
-            },
-        }],
-        title: {
-            text: 'Actualité : nuage de mots'
-        },
-        chart: {
-            backgroundColor: 'None'
-        }
-    });
-}
-
-function display_all_articles() {
-    var all_articles = []
-    for (i = 0; i < news_data['articles'].length; i++)
-        all_articles.push(i);
-    display_articles(all_articles);
-}
-
-function display_articles_from_word(word) {
-    var articles;
-    for (i in news_data['keywords']) {
-        if (news_data['keywords'][i]['word'] === word) {
-            articles = news_data['keywords'][i]['articles'];
-            break;
-        }
-    }
-    display_articles(articles);
-}
-
-function display_articles(articles) {
-    var div = $("#tableauArticles").html("");
-    div.append("<table></table");
-    var tab = $("#tableauArticles table");
-    for (i in articles) {
-        var article = news_data['articles'][articles[i]];
-        var title = article["title"];
-        var source = article["source"];
-        var url = article["url"];
-        var newLine = "<tr><td class='newspaper'>" + source + "</td><td><a target='_blank' href='" + url + "'>" + title + "</a></td></tr>"
-        tab.append(newLine);
-    }
-}
-
-
-// Chargement des données météo
-d3.json('/api/meteo', display_nvd3_graph);
-
-function display_nvd3_graph(data) {
-
-    if (data["status"] === "ok") {
-        var temperature_data = [{
-            key: 'Température',
-            values: data["data"]
-        }]
-
-        var first_date = temperature_data[0]['values'][0][0];
-
-        nv.addGraph(function () {
-
-            var chart = nv.models.lineWithFocusChart()
-                .x(function (d) {
-                    return d[0]
-                })
-                .y(function (d) {
-                    return d[1]
-                })
-                .yDomain([-5, 35])
-                .height(270)
-                .color(colors);
-
-            chart.brushExtent([new Date(first_date), new Date(first_date + 24 * 3600 * 1000)]); // 24*3600*1000ms = 1jour
-
-            chart.xAxis
-                .showMaxMin(false)
-                .tickFormat(function (d) {
-                    return d3.time.format('%H:00 (%a)')(new Date(d))
-                });
-
-            chart.x2Axis
-                .showMaxMin(false)
-                .tickFormat(function (d) {
-                    return d3.time.format('%a %-d/%-m')(new Date(d))
-                });
-
-            chart.yAxis //Chart y-axis settings
-                .showMaxMin(false)
-                .axisLabel('Température (°c)')
-                .tickFormat(d3.format('.00f'));
-
-            chart.y2Axis
-                .showMaxMin(false)
-                .ticks(false);
-
-            d3.select('#meteo svg')
-                .datum(temperature_data)
-                .call(chart);
-
-            //Update the chart when window resizes.
-            nv.utils.windowResize(chart.update);
-
-            return chart;
-        });
-    }
-}
-*/
-
-
 // Chargement des reponses du debat
 $.ajax({
     url: "/api/debat",
@@ -235,7 +81,7 @@ function display_bargraph(debat_data) {
 }
 
 
-// chargement des differents themes
+// chargement des différents themes abordés lors des autres réunions
 
 $.ajax({
     url: "/api/themes/",
@@ -263,79 +109,75 @@ function display_all_themes(themes_data) {
 }
 
 
-
-
 // chargement nrb participants en fonction de la ville
-display_participants()
-
-function display_participants() {
-    Highcharts.chart('participation', {
-    chart: {
-        type: 'column'
-    },
-    title: {
-        text: 'Nombre de participants par ville'
-    },
-
-    xAxis: {
-        type: 'category',
-        labels: {
-            rotation: -45,
-            style: {
-                fontSize: '13px',
-                fontFamily: 'Verdana, sans-serif'
-            }
-        }
-    },
-    yAxis: {
-        min: 0,
-        title: {
-            text: 'Population (millions)'
-        }
-    },
-    legend: {
-        enabled: false
-    },
-    tooltip: {
-        pointFormat: ''
-    },
-    series: [{
-        name: 'Population',
-        data: [
-            ['Tokyo', 37.33],
-            ['Delhi', 31.18],
-            ['Shanghai', 27.79],
-            ['Sao Paulo', 22.23],
-            ['Mexico City', 21.91],
-            ['Dhaka', 21.74],
-            ['Cairo', 21.32],
-            ['Beijing', 20.89],
-            ['Mumbai', 20.67],
-            ['Osaka', 19.11],
-            ['Karachi', 16.45],
-            ['Chongqing', 16.38],
-            ['Istanbul', 15.41],
-            ['Buenos Aires', 15.25],
-            ['Kolkata', 14.974],
-            ['Kinshasa', 14.970],
-            ['Lagos', 14.86],
-            ['Manila', 14.16],
-            ['Tianjin', 13.79],
-            ['Guangzhou', 13.64]
-        ],
-        dataLabels: {
-            enabled: true,
-            rotation: -90,
-            color: '#FFFFFF',
-            align: 'right',
-            format: '{point.y:.1f}', // one decimal
-            y: 10, // 10 pixels down from the top
-            style: {
-                fontSize: '13px',
-                fontFamily: 'Verdana, sans-serif'
-            }
-        }
-    }]
+$.ajax({
+    url: "/api/participation/",
+    success: display_participation
 });
+
+var participation_data;
+
+function display_participation(result3) {
+    participation_data = result3["data"];
+    display_participants(participation_data);
 }
 
+function display_participants(participation_data) {
+
+    var participation = participation_data["participation"];
+    var affichage = []
+
+    for (var i = 0; i <= 99; i++) {
+        affichage.push(participation[i])
+    }
+    console.log(affichage)
+
+    Highcharts.chart('participation', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Nombre de participants par ville'
+        },
+
+        series: [{
+            name: 'Participants',
+            data: affichage,
+            dataLabels: {
+                enabled: true,
+                rotation: -90,
+                color: '#FFFFFF',
+                align: 'right',
+                format: '{point.y:.1f}', // one decimal
+                y: 10, // 10 pixels down from the top
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        }],
+
+        xAxis: {
+            type: 'category',
+            labels: {
+                rotation: -45,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: ''
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            pointFormat: ''
+        },
+    });
+}
